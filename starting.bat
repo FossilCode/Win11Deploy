4d97989738d0a162386189e5019b6260f6a5580c
FossilCode <111083219+FossilCode@users.noreply.github.com>
2025-06-06 09:33:45 -0500

    Update starting.bat
    It was cringe before

--- starting.bat
+++ starting.bat
@@ -1,16 +1,20 @@
 @ECHO OFF
-
-ECHO Make sure Windows is UP-TO-DATE and this is being run as ADMIN.
-ECHO Program will automatically close when finished --- ~35 seconds.
-PAUSE
-ECHO Setting Fossil's custom windows settings and installing beloved programs...
-
-
-
+ECHO     ______ ____  _____ _____  ____ __ 
+ECHO    / ____// __ \/ ___// ___/ /  _// / 
+ECHO   / /_   / / / /\__ \ \__ \  / / / /  
+ECHO  / __/  / /_/ /___/ /___/ /_/ / / /___
+ECHO /_/     \____//____//____//___//_____/
+                                      
+ECHO Ensure that Windows is up to date.
+ECHO Applying custom Windows settings and installing beloved programs...
+timeout /t 10
 REM Registry tweaks
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f >nul
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" /v "VisualFXSetting" /t REG_DWORD /d 2 /f >nul
 reg add "HKCU\Control Panel\Desktop" /v DragFullWindows /t REG_SZ /d 1 /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_DWORD /d 0 /f >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Search" /v CortanaConsent /t REG_DWORD /d 0 /f >nul
+reg delete "HKEY_CURRENT_USER\Software\Microsoft\OneDrive" /f >nul
 
 REM Install software using winget
 winget install Fastfetch-cli.Fastfetch --silent
@@ -19,7 +23,6 @@ winget install Microsoft.PowerShell --silent
 winget install Audacity.Audacity --silent
 winget install Valve.Steam --silent
 winget install dotPDN.PaintDotNet --silent
-winget install StartIsBack.StartAllBack --silent
 winget install Microsoft.PowerToys --silent
 winget install Microsoft.VisualStudioCode --silent
 winget install OpenJS.NodeJS --silent
@@ -27,35 +30,10 @@ winget install VideoLAN.VLC --silent
 winget install Oracle.VirtualBox --silent
 winget install REALiX.HWiNFO --silent
 winget install Git.Git --silent
-winget install wget --silent
-
-wget https://github.com/crazy-max/WindowsSpyBlocker/releases/download/4.39.0/WindowsSpyBlocker.exe -O WindowsSpyBlocker.exe
-
-
-IF EXIST WindowsSpyBlocker.exe (
-    ECHO Running WindowsSpyBlocker...
-
-    REM Use timeout to delay input, then pipe the responses to the exe
-    timeout /t 1 >nul
-
-    (
-        echo 1
-        timeout /t 1 >nul
-        echo 1
-        timeout /t 1 >nul
-        echo 1
-        timeout /t 25 >nul
-        echo exit
-
-    ) | WindowsSpyBlocker.exe
-
-) ELSE (
-    ECHO Failed to download WindowsSpyBlocker.exe
-)
-
-echo exit
-echo Cleaning up...
-rm libs
-rm logs
-rm tmp
-rm app.conf
+winget install FortyOneLtd.AudioSwitcher --silent
+winget install Logitech.GHUB --silent
+winget install RiotGames.LeagueOfLegends.NA --silent
+winget install Python.Python.3.13 --silent
+winget install 7zip.7zip --silent
+winget install Discord.Discord --silent
+winget install qBittorrent.qBittorrent --silent
